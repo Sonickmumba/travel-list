fa2bee76fcd2b209ee81e219bbdc6ec8247a4ab6
Sonick Mumba <kingellie.mumba@gmail.com>
2023-11-17 21:15:21 +0200

    Add stats component

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -1,21 +1,21 @@
-// /* eslint-disable react/prop-types */
-// // import React from 'react'
+/* eslint-disable react/prop-types */
+// import React from 'react'
 
-// const Stats = ({ items }) => {
-//   const itemsLength = items.length;
-//   const itemsPacked = items.filter((item) => item.packed).length;
-//   const percentPacked = Math.round((itemsPacked / itemsLength) * 100);
-//   if (!items.length) return <p className="stats"><em>Start adding items to your packing list 🚀</em></p>
-//   return (
-//     <footer className="stats">
-//       <em>
-//         {percentPacked !== 100 ? 
-//         `💼 You have ${itemsLength} items on your list, and you already packed
-//         ${itemsPacked} (${itemsLength !== 0 ? percentPacked : 0}%)` : "You got everything! Ready to go ✈️"
-//         }
-//       </em>
-//     </footer>
-//   );
-// };
+const Stats = ({ items }) => {
+  const itemsLength = items.length;
+  const itemsPacked = items.filter((item) => item.packed).length;
+  const percentPacked = Math.round((itemsPacked / itemsLength) * 100);
+  if (!items.length) return <p className="stats"><em>Start adding items to your packing list 🚀</em></p>
+  return (
+    <footer className="stats">
+      <em>
+        {percentPacked !== 100 ? 
+        `💼 You have ${itemsLength} items on your list, and you already packed
+        ${itemsPacked} (${itemsLength !== 0 ? percentPacked : 0}%)` : "You got everything! Ready to go ✈️"
+        }
+      </em>
+    </footer>
+  );
+};
 
-// export default Stats;
+export default Stats;
